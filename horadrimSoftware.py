d80dbd9559e162555969de7aa768d32926a19d8c
Burak Ferit Aktan <61063293+burakaktan@users.noreply.github.com>
2022-05-28 11:36:55 +0300

    Update horadrimSoftware.py

--- horadrimSoftware.py
+++ horadrimSoftware.py
@@ -2,13 +2,17 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import sys
+from src.ddl import *
+from src.dml import *
+from bplustree.tree import BPlusTree
+from bplustree.serializer import StrSerializer
 import os
-from ddl import *
-from dml import *
+import sys
+
 
 def extend_to(l,s):
     return s + (l - len(s)) * '$'
+
 
 def remove_dollar(s):
     ns = ""
@@ -17,13 +21,19 @@ def remove_dollar(s):
             ns += ch
     return ns
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     input_file_name = sys.argv[1]
     output_file_name = sys.argv[2]
+
+    # open/close output file to cleaning
+    infor = open(output_file_name, "w")
+    infor.close()
+
     input_file = open(input_file_name, 'r')
     input_lines = input_file.readlines()
-    #create information_schema table if not exist
+    # create information_schema table if not exist
     if not os.path.exists('information_schema.txt'):
         infor = open(f"information_schema.txt", "wb+")
         add_new_page_to_information_schema(infor)
@@ -47,6 +57,8 @@ if __name__ == '__main__':
             if inp[0] == 'delete':
                 pass
             if inp[0] == 'list':
-                pass
+                list_record(inp, output_file_name)
+            if inp[0] == 'update':
+                update_record(inp)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
