b275556cbdfe790506ce5fb6e278568ae0c718bb
Burak Ferit Aktan <61063293+burakaktan@users.noreply.github.com>
2022-05-28 11:36:26 +0300

    Update ddl.py

--- ddl.py
+++ ddl.py
@@ -1,61 +1,84 @@
 from horadrimSoftware import *
-
-catalog_page_header_size = 2 + 2 + 4 # record_number + active_record_number + total_size
-catalog_page_record_number = 8
-catalog_record_header_size = 1 + 20 + 2 # valid_bit + table_name + primary_key
-catalog_record_body_size = 12*(20+3) # number of fields*(field_size + type_size)
-catalog_record_size = catalog_record_header_size + catalog_record_body_size
-catalog_page_size = catalog_page_header_size \
-                    + catalog_page_record_number*(catalog_record_header_size + catalog_record_body_size)
+from dml import *
 
 max_field_number = 12
 max_field_name_length = 20
 max_field_length = 20
 max_table_name_length = 20
 
+catalog_page_header_size = 2 + 2 + 4 # record_number + active_record_number + total_size
+catalog_page_record_number = 8
+catalog_record_header_size = 1 + max_table_name_length + 2 # valid_bit + table_name + primary_key
+catalog_record_body_size = max_field_number*(max_field_name_length+3) # number of fields*(field_size + type_size)
+catalog_record_size = catalog_record_header_size + catalog_record_body_size
+catalog_page_size = catalog_page_header_size \
+                    + catalog_page_record_number*(catalog_record_header_size + catalog_record_body_size)
+
+def extend_to(l,s):
+    return s + (l - len(s)) * '$'
+
+
+def remove_dollar(s):
+    ns = ""
+    for ch in s:
+        if ch != '$':
+            ns += ch
+    return ns
+
+
 def add_new_page_to_information_schema(infor):
-    infor.write((str(catalog_page_record_number).zfill(2) + "00" + catalog_page_size).encode("ascii"))
+    infor.write((str(catalog_page_record_number).zfill(2) + "00" + str(catalog_page_size)).encode("ascii"))
     for j in range(8):
         infor.write(("0" + (catalog_record_size-1)*'$').encode("ascii"))
+
 
 def create_type(inp):
     table_name = inp[2]
     table_name_extended = extend_to(max_table_name_length, table_name)
-    primary_key = inp[3]
+    number_of_fields = inp[3]
+    primary_key = inp[4]
     # add to information schema
     written = False
     infor = open(f"information_schema.txt", "rb+")
+    current_page = 0
     while not written:
+        print("hey")
         data = infor.read(4)[-2:].decode("ascii")
         if data == '':
+            print("add new page")
             add_new_page_to_information_schema(infor)
             continue
-        no_empty = int(data)
-        print("no empty:", no_empty)
-        if no_empty != 0:
+        no_filled = int(data)
+        print("number of filled:", no_filled)
+        if no_filled == catalog_page_record_number:
             infor.read(catalog_page_size - 4).decode("ascii")
         else:
             infor.read(4).decode("ascii")
             for i in range(catalog_page_record_number):
-                valid = int(infor.read(1).decode("ascii"))
-                if valid == 0:
+                is_filled = int(infor.read(1).decode("ascii"))
+                if is_filled == 0:
                     infor.seek(infor.tell() - 1)
                     infor.write('1'.encode("ascii"))
-                    infor.write(table_name_extended)
-                    infor.write(primary_key.zfill(2))
+                    infor.write(table_name_extended.encode("ascii"))
+                    infor.write(primary_key.zfill(2).encode("ascii"))
                     for field in range(max_field_number):
                         field_name = ""
                         field_type_name = ""
-                        if not 4 + 2 * field >= len(inp):
-                            field_name = inp[4 + 2 * field]
-                            field_type_name = inp[4 + 2 * field + 1]
+                        if not 5 + 2 * field >= len(inp):
+                            field_name = inp[5 + 2 * field]
+                            field_type_name = inp[5 + 2 * field + 1]
                         field_name_extended = extend_to(max_field_name_length, field_name)
-
                         infor.write((field_name_extended + field_type_name).encode("ascii"))
                     written = True
+                    # edit number of filled entries
+                    back_up = infor.tell()
+                    infor.seek(catalog_page_size*current_page + 2)
+                    infor.write(str(no_filled+1).zfill(2).encode("ascii"))
+                    infor.seek(back_up)
                     break
                 else:
                     infor.read(catalog_record_size-1).decode("ascii")
+            current_page += 1
     infor.close()
 
 
@@ -97,3 +120,19 @@ def delete_type(inp):
                 break
             cursor += catalog_record_size
     infor.close()
+
+
+def get_primary_key(table_name):
+    infor = open("information_schema.txt", "rb+")
+    while True:
+        data = infor.read(catalog_page_size).decode("ascii")
+        if len(data) == 0:
+            break
+        cursor = catalog_page_header_size
+        for i in range(catalog_page_record_number):
+            # if valid bit is 1 and table is the table we search
+            if data[cursor] == '1' and table_name == remove_dollar(data[(cursor+1):(cursor+1+max_table_name_length)]):
+                infor.close()
+                return int(data[cursor+1+max_table_name_length:cursor+1+max_table_name_length+2])
+    infor.close()
+    return -1
